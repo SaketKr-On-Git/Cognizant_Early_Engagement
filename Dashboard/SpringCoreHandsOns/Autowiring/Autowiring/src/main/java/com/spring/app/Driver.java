8889dcd70663d3aa890716a723ee57ff383b650d
SaketKr-On-Git <80984167+SaketKr-On-Git@users.noreply.github.com>
2022-06-06 15:06:08 +0530

    Update Driver.java

--- Dashboard/SpringCoreHandsOns/Autowiring/Autowiring/src/main/java/com/spring/app/Driver.java
+++ Dashboard/SpringCoreHandsOns/Autowiring/Autowiring/src/main/java/com/spring/app/Driver.java
@@ -7,7 +7,7 @@ public class Driver {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-		Employee employee = (Employee) context.getBean(Employee.class);
+		Employee employee = (Employee) context.getBean(Employee.class);//Employee class ka getBEan chahiye isliye Employee class ek component hai
 
 		System.out.println("EmpID : " + employee.getEmpId());
 		System.out.println("EmpName : " + employee.getEmpName());
@@ -17,3 +17,8 @@ public class Driver {
 	}
 
 }
+
+/*Notes:Iss question me mainly @Component tarike se bean create kiya gya hai.Aur xml file use nhi krne bola hai isliye @Bean tarike se bhi bean create kiya hai.
+
+
+*/
